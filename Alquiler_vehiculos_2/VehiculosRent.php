347b97c86e96da47e3cf6508dbf011fae8f8bcd9
Mi nombre <easyrobotics2@gmail-com>
2024-05-17 11:41:27 +0200

    añadir vehículo

--- Alquiler_vehiculos_2/VehiculosRent.php
+++ Alquiler_vehiculos_2/VehiculosRent.php
@@ -1,5 +1,4 @@
 <?php
-require_once 'Flota.php';
 class VehiculosRent
 {
     private string $nif;
--- Alquiler_vehiculos_2/index.php
+++ Alquiler_vehiculos_2/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'VehiculosRent.php';
+require_once 'Flota.php';
 require_once 'Coche.php';
 require_once 'Cliente.php';
 require_once 'Camion.php';
@@ -49,7 +50,7 @@ $salir = false;
 
 do{
     print "************************************************\n";
-    print "*                 Menú principal               *\n";
+    print "*            \e[32mMenú principal\e[0m                    *\n";
     print "************************************************\n";
     print "Elige una opción: (1,2...):\n";
     print "1. Añadir vehiculos a una flota\n";
@@ -81,9 +82,46 @@ function addVehiculoToFlota()
 {
     echo "Añadir una vehiculo a la flota\n";
     do{
+        //Pedir y guardar datos el Vehiculo
+        print "Matricula: ";
+        $matricula = fgets(STDIN);//TODO comprobar si el vehículo existe y mostrar en que flota está
+        print "Marca: ";
+        $marca = fgets(STDIN);
+        print "Modelo: ";
+        $modelo = fgets(STDIN);
+
+        //Elegir el tipo de vehívulo
+        print "Elige un tipo de vehículo\n";
+        print "1 Coche\n";
+        print "2 Moto\n";
+        print "3 Camión\n";
+
+        fscanf(STDIN, "%d\n", $opcion);
+        switch ($opcion){
+            case 1:
+                print "Número de plazas: \n";
+                $numero_plazas = fgets(STDIN);
+                print "Número de puertas: \n";
+                $puertas = fgets(STDIN);
+                $vehiculo = new Coche($matricula, $marca, $modelo,$numero_plazas, $puertas);
+                break;
+            case 2:
+                print "Cilindrada: ";
+                $cilindrada = fgets(STDIN);
+                $vehiculo = new Moto($matricula, $marca, $modelo, $cilindrada);
+                break;
+            case 3:
+                print "Tara: ";
+                $kg = fgets(STDIN);
+                $vehiculo = new Camion($matricula, $marca, $modelo, $kg);
+                break;
+            default:
+                echo "\e[31mOpción incorrecta! \e[0m\n";
+                return;
+        }
+        var_dump($vehiculo);
         //Mostrar las flotas
         //Selecciona una flota
-        //Pedir y guardar datos el Vehiculo
         //Añadir el vehiculo
 
     }while(true);
@@ -102,7 +140,7 @@ function mostrarVehiculosFlota()
     //Preguntar por la opción de la flota
     fscanf(STDIN, "%d\n", $opcion);
     if($opcion < 0 || $opcion > count($VR->getFlotas())){
-        print "Opción incorrecta!\n";
+        echo "\e[31mOpción incorrecta! \e[0m\n";
         return;
     }
     //listar los vehiculos de la flota que tenga el indice de la opción
